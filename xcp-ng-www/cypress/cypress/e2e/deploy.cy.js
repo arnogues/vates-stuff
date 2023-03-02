89021af9befd8d4f082392d38f5b203e01299aa0
Arnaud Gueras <arnaudgs@gmail.com>
2023-03-02 22:38:55 +0100

    Cypress

--- xcp-ng-www/cypress/cypress/e2e/deploy.cy.js
+++ xcp-ng-www/cypress/cypress/e2e/deploy.cy.js
@@ -1,10 +1,72 @@
-describe("template spec", () => {
+describe("Deploy", () => {
   beforeEach(() => {
-    // run these tests as if in a desktop
-    // browser with a 720p monitor
+    Cypress.Keyboard.defaults({
+      keystrokeDelay: 0,
+    });
+    Cypress.Screenshot.defaults({
+      overwrite: true,
+    });
     cy.viewport(1280, 1280);
-  });
-  it("passes", () => {
     cy.visit("http://localhost:8000/XCP-ng-index.html");
+  });
+  it("Xen Orchestra Page", () => {
+    cy.get("html").screenshot();
+  });
+
+  it("close the modal", () => {
+    cy.contains("Quick deploy").click();
+    // button X of modal
+    cy.get(".modal").should("be.visible");
+    cy.get(".close-modal").click();
+    cy.get(".modal").should("not.be.visible");
+    cy.contains("Quick deploy").click();
+    cy.get(".modal").should("be.visible");
+    cy.contains("Close").click();
+    cy.get(".modal").should("not.be.visible");
+  });
+
+  it("Xen Orchestra Quick Deploy Spinner", () => {
+    cy.clock();
+
+    // mock session.ligin_with_password
+    require("./mocks/jsonRpc.js");
+    cy.contains("Quick deploy").click();
+
+    // connect
+    cy.get("#pwd").type("password");
+    cy.contains("Connect").click();
+
+    // Xen Orchestra Quick Deploy form
+    cy.get("#ip").type("127.0.0.1:8000");
+    cy.get("#gateway").type("127.0.0.1");
+    cy.get("#dns").type("1.1.0.0");
+    cy.contains("Next").click();
+
+    // Xen orchestra Quick Deploy form 2
+    cy.get("#updaterEmail").type("update@vates.fr");
+    cy.get("#updaterPwd").type("password");
+    cy.get("#xoaPwd").type("password");
+
+    cy.intercept("POST", "api", {
+      status: 200,
+      delay: 1000,
+      body: {
+        result: "rue",
+      },
+    });
+
+    cy.contains("Deploy").click();
+    cy.contains("Deploy").first().screenshot();
+    cy.contains("XOA is ready! Redirecting…").should("be.visible");
+    cy.tick(3000);
+    cy.origin("http://127.0.0.1:8000", () => {
+      cy.location("href").should("match", /127.0.0.1/);
+    });
+
+    // cy.location("href").then((str) => {
+    //   console.log(str);
+    //   //localhost:8000$
+    //   //http: debugger;
+    // }); //.should("match", /localhost/);
   });
 });
